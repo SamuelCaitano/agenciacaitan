834d8389160f50a7667ff584bb818c22812bcf9f
Samuel Caitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-04-17 18:28:28 -0300

    Update head, include google tag manager and meta tags

--- resources/views/site/components/head.blade.php
+++ resources/views/site/components/head.blade.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>New Project - Welcome to the new age</title>
-  <meta http-equiv="Content-Language" content="en">
+  <title>Agência Caitan - Sua empresa no digital</title>
+  <meta http-equiv="Content-Language" content="pt-br">
   <meta name="language" content="portuguese, BR, PT">
   <meta http-equiv="X-UA-Compatible" content="ie=edge,chrome=1">
   <meta name="author" content="Samuel Caitano">
   <meta content="Samuel Caitano" property="og:author">
   {{-- WARNING: Adicionar uma descrição melhor --}}
-  <meta name="description" content="New Project - Welcome to the new age">
+  <meta name="description" content="Agência Caitan - Sua empresa no digital">
   {{-- WARNING: adicionar a URL da página --}}
   <link rel="canonical" href="http://">
   <meta property="og:locale" content="pt_BR">
   <meta property="og:type" content="website">
-  <meta property="og:title" content="New Project - Welcome to the new age">
+  <meta property="og:title" content="Agência Caitan - Sua empresa no digital">
   {{-- WARNING: Adicionar a IMAGEM que deve ser exibida ao compartilhar por link --}}
   <meta property="og:image" content="https://">
   {{-- WARNING: Adicionar uma descrição melhor --}}
-  <meta property="og:description" content="New Project - Welcome to the new age">
+  <meta property="og:description" content="Agência Caitan - Sua empresa no digital">
   {{-- WARNING: Adicionar a URL da página --}}
   <meta property="og:url" content="http://">
-  <meta property="og:site_name" content="New Project - Welcome to the new age">
+  <meta property="og:site_name" content="Agência Caitan - Sua empresa no digital">
   <meta name="author" content="Samuel Caitano">
   <meta name="twitter:card" content="summary_large_image">
   {{-- Favicon --}}
-  <link rel="shortcut icon" type="image/x-icon" href="/assets/img/logo/favicon.png" type="image/x-icon">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png" type="image/x-icon">
 
   {{-- =========== Frameworks and Plugins ======== --}}
 
@@ -44,20 +44,45 @@
     integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+  <link rel="stylesheet" href="assets/css/magnific-popup.css">
   <link rel="stylesheet" href="assets/css/odometer-theme-default.css">
   {{-- Owl-Carousel --}}
   <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
   <link rel="stylesheet" href="assets/css/styles.css">
 
   {{-- Main CSS --}}
-  <link rel="stylesheet" href="/assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
   {{-- JQuery v.3.6.0 --}}
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
     integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   {{-- IMPORTANT: Navbar responsive --}}
-  <script src="\assets\js\jquery.main.js"></script> 
+  <script src="assets/js/jquery.main.js"></script>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-4DB7S556R4"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4DB7S556R4');
+  </script>
+
+  {{-- Google Tag Manager --}}
+  <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WRLQQ49');
+  </script>
+  {{-- End Google Tag Manager --}}
 </head>
 
 <body id="body" data-spy="scroll" data-target=".navbar" class="has-loading-screen">
+
+  {{-- Google Tag Manager (noscript) --}}
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRLQQ49" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  {{-- End Google Tag Manager (noscript) --}}  
